30b2379d5c15595b0698e2f0894e29ca998b0f31
Tian <11132373+tianjiayan@user.noreply.gitee.com>
2022-06-26 15:35:48 +0800

    feat(add): 新盒子

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="login-container"></div>
+  <div class="login-container">
+    <div class="login-box">
+        <h3>登录</h3>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,5 +15,10 @@ export default {}
   width: 100%;
   height: 100%;
   background-color: #2d3a4b;
+}
+.login-box{
+  width: 500px;
+  height: 500px;
+  background-color: white;
 }
 </style>
