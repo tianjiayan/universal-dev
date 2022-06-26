e02a19e5f2e313a196a0bb81162b80190c41b510
Tian <11132373+tianjiayan@user.noreply.gitee.com>
2022-06-26 20:03:53 +0800

    feat(svg): svg修改配置

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import './styles/index.scss'
-// import '@/assets/icons'
-// import axios from './request'
+import './iconfont/iconfont.css'
+import installIcons from './icons/index.js'
 
 const app = createApp(App)
+installIcons(app)
+
 app.use(store).use(router).use(ElementPlus).mount('#app')
